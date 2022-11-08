2d8025b3eb658a038d02d71ce7f780c15979b7cc
Mekhi Wood-Brock <mekhiwb966@gmail.com>
2022-11-07 19:38:54 -0500

    moved the inteface inside of MyHeap.java

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -1,5 +1,57 @@
-public class MyHeap {
+interface MyQueue{
+    void addMovie();
+    void clearAll();
+    void isEmpty();
+    void peekMovie();
+    void pushMovie();
+    void removeMovie();
+    void size();
+
+}
+public class MyHeap implements MyQueue {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    }
+
+    @Override
+    public void addMovie() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void clearAll() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void isEmpty() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void peekMovie() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pushMovie() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeMovie() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void size() {
+        // TODO Auto-generated method stub
+        
     }
 }
